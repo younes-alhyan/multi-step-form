a62be559fdf00a72e10287c215c595158c59d65b
Yokai-47 <gameryounes555@gmail.com>
2025-08-26 01:46:16 +0100

    Add ThankYou component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import Bar from "./components/Bar";
 import Form from "./components/Form";
+import ThankYou from "./components/ThankYou";
 import "./App.css";
 
 function App() {
   const [step, setStep] = useState(1);
+
   return (
     <div className="App">
-      <Bar step={step}></Bar>
-      <Form step={step} setStep={setStep}></Form>
+      <Bar step={step} />
+      {step <= 4 ? <Form step={step} setStep={setStep} /> : <ThankYou />}
     </div>
   );
 }
